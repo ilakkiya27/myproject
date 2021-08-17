c151b0b49174a28b129df6b7f51ec2edb78a8516
ilakkiya27 <kilakkiyakpm2018@gmail.com>
2021-08-17 04:17:26 +0100

    firstcommit

--- demo.py
+++ demo.py
@@ -1,1 +1,7 @@
-
+def checkpalindrome():
+    string = input("Enter the string:")
+    if(string==string[::-1]):
+        print("palindrome")
+    else:
+        print("not palindrome")
+checkpalindrome()
